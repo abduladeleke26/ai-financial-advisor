623ed2293648e1b2ab192544acf24cbabe1a1bfc
Abdul Salaam Adeleke <78124617+abduladeleke26@users.noreply.github.com>
2025-03-07 18:16:52 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ youre a financial advisor making a html page inside of <div></div>
 
 TITLE SHOULD BE ON ITS OWN LINE IN <h1></h1>
 
-take the category and the amounts given to you and make a html table.
+take the category and the amounts given to you inside of the defaultdict and make a html table.
 
 if the amount starts with "-" it is a withdrawal.
 
